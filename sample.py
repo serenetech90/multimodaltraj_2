a1597775f2047931b03d3c19a8044e31c4cc7d74
serenetech90 <serenetech90@gmail.com>
2019-11-19 17:10:21 +0800

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import pickle
 from models import g2k_lstm_mcr as mcr
 import argparse
-import nri_learned as nri
+import time
 import networkx_graph as nx_g
 import argParser as argsParser
 import load_traj as load
@@ -27,39 +27,58 @@ def get_mean_error(predicted_traj, true_traj, observed_length, maxNumPeds):
     observed_length : The length of trajectory observed
     '''
     # The data structure to store all errors
-    error = np.zeros(len(true_traj) - observed_length)
+
+    try:
+        true_traj = np.transpose(true_traj,(1,0,2))
+        predicted_traj = np.transpose(predicted_traj, (1, 0, 2))
+    except ValueError:
+        print()
+
+    final_timestep_error = 0
+    error = np.zeros(shape=(len(true_traj) - observed_length, 2))
+    fde_error = []
     # For each point in the predicted part of the trajectory
+    counter = 0
     for i in range(observed_length, len(true_traj)):
         # The predicted position. This will be a maxNumPeds x 3 matrix
         pred_pos = predicted_traj[i, :]
         # The true position. This will be a maxNumPeds x 3 matrix
         true_pos = true_traj[i, :]
         timestep_error = 0
-        counter = 0
-        for j in range(maxNumPeds):
-            if true_pos[j, 0] == 0:
-                # Non-existent ped
-                continue
-            elif pred_pos[j, 0] == 0:
-                # Ped comes in the prediction time. Not seen in observed part
-                continue
-            else:
-                if true_pos[j, 1] > 1 or true_pos[j, 1] < 0:
-                    continue
-                elif true_pos[j, 2] > 1 or true_pos[j, 2] < 0:
-                    continue
 
-                timestep_error += np.linalg.norm(true_pos[j, [1, 2]] - pred_pos[j, [1, 2]])
-                counter += 1
+        fde_counter = 0
+        for j in range(maxNumPeds):
+            # if true_pos[j, 0] == 0:
+            #     # Non-existent ped
+            #     continue
+            # elif pred_pos[j, 0] == 0:
+            #     # Ped comes in the prediction time. Not seen in observed part
+            #     continue
+            # else:
+            #     if true_pos[j, 1] > 1 or true_pos[j, 1] < 0:
+            #         continue
+                # elif true_pos[j, 2] > 1 or true_pos[j, 2] < 0:
+                #     continue
+                # timestep_error += np.linalg.norm(true_pos[j, [1, 2]] - pred_pos[j, [1, 2]])
+            timestep_error += np.subtract(true_pos[j], pred_pos[j]) #np.linalg.norm(true_pos[j] - pred_pos[j])
+
+            if i == len(true_traj)-1:
+                final_timestep_error += np.subtract(true_pos[j], pred_pos[j])
+                fde_error.append(np.subtract(true_pos[j], pred_pos[j]))
+                fde_counter += 1
+
+            counter += 1
 
         if counter != 0:
-            error[i - observed_length] = timestep_error / counter
+            error[i - observed_length] = timestep_error #/ counter
 
         # The euclidean distance is the error
         # error[i-observed_length] = np.linalg.norm(true_pos - pred_pos)
 
     # Return the mean error
-    return np.mean(error)
+    print('ADE = ', np.mean(np.linalg.norm(error,ord=2,axis=1)/counter))
+    print('FDE = ', np.mean(np.linalg.norm(np.stack(fde_error), ord=2, axis=1) / fde_counter))
+    return np.mean(np.linalg.norm(error,ord=2,axis=1)/counter), np.mean(np.linalg.norm(np.stack(fde_error), ord=2, axis=1) / fde_counter) , counter # np.mean(error)
 
 
 def main():
@@ -75,7 +94,7 @@ def main():
     parser.add_argument('--pred_length', type=int, default=12,
                         help='Predicted length of the trajectory')
     # Test dataset
-    parser.add_argument('--test_dataset', type=int, default=2,
+    parser.add_argument('--test_dataset', type=int, default=4,
                         help='Dataset to be tested on')
 
     # Model to be loaded
@@ -86,7 +105,7 @@ def main():
     sample_args = parser.parse_args()
 
     # Save directory
-    save_directory = '/home/siri0005/Documents/multimodaltraj_2/save'
+    save_directory = '/home/serene/PycharmProjects/multimodaltraj_2/save'
 
     # Define the path for the config file for saved args
     # with open(os.path.join(save_directory, 'social_config.pkl'), 'rb') as f:
@@ -110,7 +129,9 @@ def main():
     vislet_emb = None
     dim = int(args.neighborhood_size / args.grid_size)
     # Variable to maintain total error
-    total_error = 0
+    total_error = []
+    total_ped_ctr = 0
+    final_error = []
 
     # For each batch
     for b in range(data_loader.num_batches):
@@ -121,12 +142,22 @@ def main():
         else:
             vislet_past = 1
 
+        if b == 38:
+            print()
+        # for i in range(args.obs_len):
+
         graph = nx_g.online_graph(args)
         graph_t = graph.ConstructGraph(current_batch=x_batch, framenum=frame, future_traj=y_batch)
-
         batch_v = list(graph_t.get_node_attr(param='node_pos_list').values())
-        batch_v = np.transpose(batch_v)
+        if len(np.array(batch_v).shape) > 1:
+            batch_v = np.array(batch_v)[:, frame:frame+args.obs_len]
+        else:
+            batch_v = np.array(batch_v)[frame:frame + args.obs_len]
+            break
+        #     batch_v = np.array(batch_v)[frame:frame+args.obs_len]
 
+        batch_v = np.linalg.norm(batch_v,axis=2)
+        batch_v = np.transpose(batch_v)
         # Batch size is 1
         # x_batch, y_batch, d_batch, x_batch_grid, ped_fr_batch = x[0], y[0], d[0], x_grid[0], fr_ped[0]
         num_nodes = batch_v.shape[1]
@@ -145,7 +176,7 @@ def main():
             grid_size=args.grid_size,
             dim=args.num_freq_blocks)
 
-        stat_mask = tf.zeros(shape=(dim, args.num_freq_blocks), dtype=tf.float64)
+        stat_mask = tf.random_normal(shape=(dim, args.num_freq_blocks), dtype=tf.float64)
         stat_mask += tf.expand_dims(tf.range(start=0, limit=1, delta=0.125, dtype=tf.float64), axis=1)
 
         vislet = data_loader.vislet[:, frame:frame + num_nodes]
@@ -156,7 +187,7 @@ def main():
                                    trainable=True, dtype=tf.float64)
 
             weight_ii = tf.Variable(name='weight_ii',
-                                    initial_value=init_w(shape=(args.num_freq_blocks, args.input_size)),
+                                    initial_value=init_w(shape=(args.num_freq_blocks, args.obs_len)),
                                     trainable=True, dtype=tf.float64)
 
         tf.initialize_variables(var_list=[weight_i, weight_ii]).run()
@@ -220,31 +251,43 @@ def main():
         # obs_traj is an array of shape obs_length x maxNumPeds x 3
 
         print("********************** SAMPLING A NEW TRAJECTORY", b, "******************************")
-
+        start = time.time()
         # compute relative locations and relative vislets
-        st_embeddings, hidden_state, ng_output, c_hidden_state = \
-            sess.run([nghood_enc.input, nghood_enc.state_f00_b00_c,
-                      nghood_enc.output, nghood_enc.c_hidden_state],
+        start_1 = time.time()
+
+        # st_embeddings, hidden_state, ng_output, c_hidden_state = \
+        st_embeddings, hidden_state, ng_output, c_hidden_state = sess.run([nghood_enc.input, nghood_enc.state_f00_b00_c,
+                      nghood_enc.ng_output, nghood_enc.c_hidden_state],
                      feed_dict={nghood_enc.input: inputs.eval(),
                                 nghood_enc.state_f00_b00_c: hidden_state,
-                                nghood_enc.output: out_init.eval(),
+                                nghood_enc.ng_output: out_init.eval(),
                                 nghood_enc.c_hidden_state: c_hidden_init.eval()})
+        print('wall-clock time taken by social mask grid = {0} seconds'.format(time.time() - start_1))
+        # using time.time() makes more sense than time.clock as car will be running other software sub-systems
+        # beside the predictive algorithm.
 
         # Soft-attention mechanism equipped with static grid
-        static_mask, social_frame = \
-            sess.run([static_mask, output],
-                     feed_dict={static_mask: static_mask_nd,
-                                social_frame: ng_output,
-                                state_f00_b00_c: c_soft * hidden_state,
-                                output: out_init.eval(),
-                                c_hidden_states: c_hidden_init.eval()
-                                })
+        start_2 = time.time()
+        # static_mask, social_frame = sess.run([static_mask, output],
+        #              feed_dict={static_mask: static_mask_nd,
+        #                         social_frame: ng_output,
+        #                         state_f00_b00_c: c_soft * hidden_state,
+        #                         output: out_init.eval(),
+        #                         c_hidden_states: c_hidden_init.eval()
+        #                         })
 
-        input = tf.matmul(b=static_mask, a=social_frame).eval()
-        combined_ngh, hidden_state = sess.run([stat_ngh.input, stat_ngh.hidden_state],
-                                              feed_dict={stat_ngh.input: input,
-                                                         stat_ngh.hidden_state: hidden_state})
+        print('wall-clock time taken by static mask grid = {0} seconds'.format(time.time() - start_2))
+        # tf.while_loop(nghood_enc, stat_ngh, parallel_iterations=1, loop_vars=[])
+        start_3 = time.time()
+        # input = tf.matmul(b=static_mask, a=social_frame).eval()
+        # input = tf.matmul(b=static_mask_nd, a=social_frame).eval()
+        # combined_ngh, hidden_state = sess.run([stat_ngh.input, stat_ngh.hidden_state],
+        #                                       feed_dict={stat_ngh.input: input,
+        #                                                  stat_ngh.hidden_state: hidden_state})
 
+        print('wall-clock time taken by combined mask grid = {0} seconds'.format(time.time() - start_3))
+
+        start_4 = time.time()
         reg_ng = np.transpose(args.lambda_param * np.transpose(ng_output))
 
         tf.initialize_variables(var_list=[krnl_mdl.weight_r, krnl_mdl.embed_vis, krnl_mdl.weight_v, krnl_mdl.bias_v]).run()
@@ -259,21 +302,31 @@ def main():
                          krnl_mdl.hidden_states: hidden_state,
                          # krnl_mdl.lambda_reg: args.lambda_reg,
                          krnl_mdl.pred_path_band: tf.random_normal(shape=(2, 12, num_nodes)).eval()})
+        print('wall-clock time taken by predictive kernel = {0} seconds'.format(time.time() - start_4))
 
         # for (i, idx) in zip(krnl_mdl.cost.eval(), range(len(hidden_state))):
-        attn = tf.exp(krnl_mdl.attn) / tf.cumsum(tf.exp(krnl_mdl.attn))
+        start_attn = time.time()
+        # attn = tf.exp(krnl_mdl.attn) / tf.cumsum(tf.exp(krnl_mdl.attn))
 
         # update with weighted hidden states
         krnl_mdl.hidden_states = tf.nn.softmax(
             krnl_mdl.hidden_states)  # weigh hidden states then send them back to glstm on next step
         hidden_state = krnl_mdl.hidden_states.eval()
-        c_soft = np.sum(np.matmul(attn.eval(), hidden_state))
 
+        # c_soft = np.sum(np.matmul(attn.eval(), hidden_state))
+        print('Relational inference calculation took= {0} seconds'.format(time.time()-start_attn))
+
+        end = time.time()
+        print('Multi-Cued model (MCR) sampling time = {0} seconds'.format(end - start))
         complete_traj = np.transpose(complete_traj, (2, 1, 0))
-
+        # sampling time = [0-9].[0-9]+
         # complete_traj is an array of shape (obs_length+pred_length) x maxNumPeds x 3
-        y_batch = np.stack(list(graph_t.get_node_attr(param='targets').values())).squeeze()
-        total_error += get_mean_error(complete_traj, y_batch, sample_args.obs_length, maxNumPeds=num_nodes)
+        y_batch = np.stack(list(graph_t.get_node_attr(param='targets').values())).squeeze(1)
+        curr_error, final_step_error, curr_ctr = get_mean_error(complete_traj, y_batch, sample_args.obs_length, maxNumPeds=num_nodes)
+        total_ped_ctr += curr_ctr
+        final_error.append(final_step_error)
+
+        total_error.append(curr_error)
 
         print("Processed trajectory number : ", b, "out of ", data_loader.num_batches, " trajectories")
 
@@ -282,7 +335,8 @@ def main():
         results.append((x_batch, complete_traj, sample_args.obs_length))
 
     # Print the mean error across all the batches
-    print("Total mean error of the model is ", total_error / data_loader.num_batches)
+    print("Total mean error of the model is ",np.mean(total_error))#(total_ped_ctr)) #
+    print("Total final error of the model is ", np.mean(final_error))#(final_error))
 
     print("Saving results")
     with open(os.path.join(save_directory, 'social_results.pkl'), 'wb') as f:
@@ -293,7 +347,7 @@ def main():
     results = pickle.load(f)
     print("Saving results as .mat")
     filesave = 'VisualizeUtils/' + 'tc_real_head_pose_z1_z2_ucy_social_lstm.mat'
-    scipy.io.savemat(filesave, mdict={'data': results})
+    # scipy.io.savemat(filesave, mdict={'data': results})
 
 
 if __name__ == '__main__':
